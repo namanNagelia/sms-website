669fd0dff780d323dc22a672542819aa025f0a3c
Miguel Ventura <miguelventura1123@gmail.com>
2024-03-18 22:38:34 -0400

    News Refactoring
    Made it easier to edit the news prop

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,48 +3,56 @@ import Image from "next/image";
 import { useState } from "react";
 import NewsArticles from "@/components/homePage/newsArticles";
 
-const newsDataDefault = [
-  {
-    title: "Golden Bears Add Two Signees in 2023 Class",
-    date: "2022-11-09",
-    premium: false,
-    imageURL:
-      "https://d195hqvwre713v.cloudfront.net/images/2022/11/9/2223MBB-NSD-Twitter-both_copy_LogDa.jpg",
-    redirectURL:
-      "https://calbears.com/news/2022/11/9/mens-basketball-golden-bears-add-two-signees-in-2023-class.aspx",
-  },
-  {
-    title: "USC adds transfer Sheppard, high school guard Bradley",
-    date: "Not Provided",
-    premium: false,
-    imageURL:
-      "https://dims.apnews.com/dims4/default/e154906/2147483647/strip/true/crop/3000x2000+0+0/resize/599x399!/quality/90/?url=https%3A%2F%2Fstorage.googleapis.com%2Fafs-prod%2Fmedia%2Fc249631e4aaa49679af3795d27bb0cc3%2F3000.jpeg",
-    redirectURL:
-      "https://apnews.com/article/sports-mens-college-basketball-college-basketball-southern-california-trojans-mens-basketball-california-c1e9175c6b0a89dc0d533f042473caac",
-  },
-];
-
+type newsProp = {
+  title: string;
+  date: string;
+  premium: boolean;
+  imageURL: string;
+  redirectURL: string;
+}
 
 export default function Home() {
+  const newsDataDefault = [
+    {
+      title: "Golden Bears Add Two Signees in 2023 Class",
+      date: "2022-11-09",
+      premium: false,
+      imageURL:
+        "https://d195hqvwre713v.cloudfront.net/images/2022/11/9/2223MBB-NSD-Twitter-both_copy_LogDa.jpg",
+      redirectURL:
+        "https://calbears.com/news/2022/11/9/mens-basketball-golden-bears-add-two-signees-in-2023-class.aspx",
+    },
+    {
+      title: "USC adds transfer Sheppard, high school guard Bradley",
+      date: "Not Provided",
+      premium: false,
+      imageURL:
+        "https://dims.apnews.com/dims4/default/e154906/2147483647/strip/true/crop/3000x2000+0+0/resize/599x399!/quality/90/?url=https%3A%2F%2Fstorage.googleapis.com%2Fafs-prod%2Fmedia%2Fc249631e4aaa49679af3795d27bb0cc3%2F3000.jpeg",
+      redirectURL:
+        "https://apnews.com/article/sports-mens-college-basketball-college-basketball-southern-california-trojans-mens-basketball-california-c1e9175c6b0a89dc0d533f042473caac",
+    },
+  ];
+
   return (
     <div className="flex-row w-full h-full justify-center items-center text-center">
       <div id="Padding" className="h-20"></div>
-      <NewsLetter />
+      <NewsLetter news={newsDataDefault}/>
       <Games />
       <TopPlayers />
     </div>
   );
 }
 
-export function NewsLetter() {
+export function NewsLetter(props : {news: newsProp[]}) {
+  // Change this later, Cause this is giving me a stroke
   const newsData = Array(4).fill(null).map((_, index) => (
     <div className="justify-center items-center">
       <NewsArticles
-        title={newsDataDefault[0].title}
-        date={newsDataDefault[0].date}
-        premium={false}
-        imageURL={newsDataDefault[0].imageURL}
-        redirectURL={newsDataDefault[0].redirectURL}
+        title={props.news[0].title}
+        date={props.news[0].date}
+        premium={props.news[0].premium}
+        imageURL={props.news[0].imageURL}
+        redirectURL={props.news[0].redirectURL}
       />
     </div>
   ));
