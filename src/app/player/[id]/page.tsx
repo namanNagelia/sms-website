2e569a0c064dbafe72d6bdec537540aa285a1cee
Miguel Ventura <miguelventura1123@gmail.com>
2024-03-19 22:15:48 -0400

    Stuff
    (New Player Page Banner)

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Image from "next/image";
+import Logo from "@/../public/SMSLogo.png";
 
 export default function PlayerPage() {
   return (
-    <div>
-      <h1>Player Page</h1>
+    <div className="h-full w-full flex flex-col text-center">
+      <PlayerNameBanner />
+      <div className="h-full w-full"></div>
+    </div>
+  );
+}
+
+const PlayerNameBanner = () => {
+  return (
+    <div className="flex player-header absolute flex-row bg-primary items-end space-x-4 player-card">
+      <Image src={Logo} alt={"Broski"} width={108} height={108} />
+      <div className="flex flex-col mb-5 items-start text-brandWhite">
+        <div className="font-dinCondensed text-3xl">Jamal Marshal</div>
+        <div className="font-dinCondensed text-lg">Data Points</div>
+
+      </div>
     </div>
   );
 }
