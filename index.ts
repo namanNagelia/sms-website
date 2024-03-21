75e31873f04c3163561c5347d3efa6574b9f6f83
namanNagelia <nagelia.naman@gmail.com>
2024-03-21 18:07:15 -0400

    fixed Prisma

--- index.ts
+++ index.ts
@@ -2,48 +2,15 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-// POST function
-async function postNews(newsData: any) {
-  const newNews = await prisma.news.create({
-    data: newsData,
+async function getFirstFiveRows() {
+  const rows = await prisma.Player_Roster_Phase1.findMany({
+    take: 5,
   });
-  console.log(newNews);
-  return newNews;
+  console.log(rows);
+  return rows;
 }
 
-// GET function
-async function getNews(newsId: number) {
-  const news = await prisma.news.findUnique({
-    where: {
-      News_Id: newsId,
-    },
-  });
-  console.log(news);
-  return news;
-}
-
-// DELETE function
-async function deleteNews(newsId: number) {
-  const deletedNews = await prisma.news.delete({
-    where: {
-      News_Id: newsId,
-    },
-  });
-  console.log(deletedNews);
-  return deletedNews;
-}
-
-// Usage
-const newsData = {
-  News_Id: 1,
-  News_Match_Id: 2,
-  News_Team_Id: 3,
-  News_Player_Id: 4,
-  News_Name: "Sample News",
-};
-
-getNews(1)
-  .then(() => deleteNews(1))
+getFirstFiveRows()
   .catch((e) => {
     throw e;
   })
