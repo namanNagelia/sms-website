144ef05212b1aa1f7ecb6d1ee83c49be4f7af71d
namanNagelia <nagelia.naman@gmail.com>
2024-03-24 13:06:08 -0400

    new route

---
+++ src/app/api/getPlayers/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+export async function GET(request: Request) {
+  const firstPlayer = await prisma.sms_user.findMany();
+  return NextResponse.json({ firstPlayer });
+}
