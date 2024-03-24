3f41efac31361c04368f7afb6f5a6726d3038ddd
namanNagelia <nagelia.naman@gmail.com>
2024-03-23 20:27:41 -0400

    comment out

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,25 +1,24 @@
 import React from "react";
 import GamePageUI from "./ui";
 
-const fetchGameDetails = async (id: string) => {
-  try {
-    const url =
-      process.env.DEV === "0"
-        ? `http://localhost:3000/api/specificGameID?id=${id}`
-        : `https://sms-website-sigma.vercel.app.com/api/specificGameID?id=${id}`;
-    const res = await fetch(url, {
-      next: { revalidate: 1 },
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const fetchGameDetails = async (id: string) => {
+//   try {
+//     const url =
+//       process.env.DEV === "0"
+//         ? `http://localhost:3000/api/specificGameID?id=${id}`
+//         : `https://main.d1ad0hew81s5fh.amplifyapp.com/api/specificGameID?id=${id}`;
+//     const res = await fetch(url, {
+//       next: { revalidate: 1 },
+//     });
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 export default async function GamePage({ params }: { params: { id: string } }) {
-  const gameDetails = await fetchGameDetails(params.id);
-  console.log(gameDetails);
+  // const gameDetails = await fetchGameDetails(params.id);
   // return <GamePageUI gameDetailsStats={gameDetails} />;
   return <div>Test</div>;
 }
--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -1,80 +1,79 @@
 import React from "react";
 import PlayerPageUI from "./ui";
 
-const fetchPlayerProfile = async (id: string) => {
-  try {
-    const url =
-      process.env.DEV === "0"
-        ? `http://localhost:3000/api/specificPlayer?id=${id}`
-        : `https://sms-website-sigma.vercel.app/api/specificPlayer?id=${id}`;
-    const res = await fetch(url, {
-      next: { revalidate: 1 },
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const fetchPlayerProfile = async (id: string) => {
+//   try {
+//     const url =
+//       process.env.DEV === "0"
+//         ? `http://localhost:3000/api/specificPlayer?id=${id}`
+//         : `https://sms-website-sigma.vercel.app/api/specificPlayer?id=${id}`;
+//     const res = await fetch(url, {
+//       next: { revalidate: 1 },
+//     });
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-const fetchAverages = async (id: string) => {
-  try {
-    const url =
-      process.env.DEV === "0"
-        ? `http://localhost:3000/api/playerStatsSeason?id=${id}`
-        : `https://sms-website-sigma.vercel.app/api/playerStatsSeason?id=${id}`;
-    const res = await fetch(url, {
-      next: { revalidate: 1 },
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const fetchAverages = async (id: string) => {
+//   try {
+//     const url =
+//       process.env.DEV === "0"
+//         ? `http://localhost:3000/api/playerStatsSeason?id=${id}`
+//         : `https://sms-website-sigma.vercel.app/api/playerStatsSeason?id=${id}`;
+//     const res = await fetch(url, {
+//       next: { revalidate: 1 },
+//     });
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-const fetchShotChartData = async (id: string) => {
-  try {
-    const url =
-      process.env.DEV === "0"
-        ? `http://localhost:3000/api/seasonShotLocations?id=${id}`
-        : `https://sms-website-sigma.vercel.app/api/seasonShotLocations?id=${id}`;
-    const res = await fetch(url, {
-      next: { revalidate: 1 },
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const fetchShotChartData = async (id: string) => {
+//   try {
+//     const url =
+//       process.env.DEV === "0"
+//         ? `http://localhost:3000/api/seasonShotLocations?id=${id}`
+//         : `https://sms-website-sigma.vercel.app/api/seasonShotLocations?id=${id}`;
+//     const res = await fetch(url, {
+//       next: { revalidate: 1 },
+//     });
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-const fetchShotLocations = async (id: string) => {
-  try {
-    const url =
-      process.env.DEV === "0"
-        ? `http://localhost:3000/api/shotTypes?id=${id}`
-        : `https://sms-website-sigma.vercel.app/api/shotTypes?id=${id}`;
-    const res = await fetch(url, {
-      next: { revalidate: 1 },
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const fetchShotLocations = async (id: string) => {
+//   try {
+//     const url =
+//       process.env.DEV === "0"
+//         ? `http://localhost:3000/api/shotTypes?id=${id}`
+//         : `https://sms-website-sigma.vercel.app/api/shotTypes?id=${id}`;
+//     const res = await fetch(url, {
+//       next: { revalidate: 1 },
+//     });
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 export default async function PlayerPage({
   params,
 }: {
   params: { id: string };
 }) {
-  const playerProfile = await fetchPlayerProfile(params.id);
-  const seasonAverages = await fetchAverages(params.id);
-  const shotChartData = await fetchShotChartData(params.id);
-  const shotLocations = await fetchShotLocations(params.id);
-  console.log(playerProfile);
+  // const playerProfile = await fetchPlayerProfile(params.id);
+  // const seasonAverages = await fetchAverages(params.id);
+  // const shotChartData = await fetchShotChartData(params.id);
+  // const shotLocations = await fetchShotLocations(params.id);
   // return (
   //   <PlayerPageUI
   //     profileStats={playerProfile}
