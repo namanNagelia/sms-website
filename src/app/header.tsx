db1003a7d677cbf6f547b741d9117ec091175175
namanNagelia <nagelia.naman@gmail.com>
2024-03-23 20:29:34 -0400

    Update header.tsx

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import HeaderUI from "./headerui";
-const fetchPlayer = async () => {
-  try {
-    const url =
-      process.env.DEV === "0"
-        ? "http://localhost:3000/api/allPlayers"
-        : "https://main.d1ad0hew81s5fh.amplifyapp.com/api/allPlayers";
-    const res = await fetch(url, {
-      next: { revalidate: 1 },
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const fetchPlayer = async () => {
+//   try {
+//     const url =
+//       process.env.DEV === "0"
+//         ? "http://localhost:3000/api/allPlayers"
+//         : "https://sms-website-sigma.vercel.app/api/allPlayers";
+//     const res = await fetch(url, {
+//       next: { revalidate: 1 },
+//     });
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
 export default async function Header() {
-  const playerData = await fetchPlayer();
-  return <HeaderUI playerData={playerData} />;
+  // const playerData = await fetchPlayer();
+  // return <HeaderUI playerData={playerData} />;
+  return <div>Test</div>;
 }
