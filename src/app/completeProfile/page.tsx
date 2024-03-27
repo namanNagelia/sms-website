d17afddf04db4f9befa9ef29964a658ba840f0e0
namanNagelia <nagelia.naman@gmail.com>
2024-03-26 21:19:02 -0400

    api

--- src/app/completeProfile/page.tsx
+++ src/app/completeProfile/page.tsx
@@ -1,22 +1,22 @@
 import React from "react";
 import CompleteProfilePageUI from "./ui";
 
-const fetchAllTeams = async () => {
-  try {
-    const url =
-      process.env.DEV === "0"
-        ? "http://localhost:3000/api/getAllTeams"
-        : "https://sms-website-sigma.vercel.app/api/getAllTeams";
-    const res = await fetch(url, {
-      next: { revalidate: 1000 * 60 * 60 },
-    });
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const fetchAllTeams = async () => {
+//   try {
+//     const url =
+//       process.env.DEV === "0"
+//         ? "http://localhost:3000/api/getAllTeams"
+//         : "https://sms-website-sigma.vercel.app/api/getAllTeams";
+//     const res = await fetch(url, {
+//       next: { revalidate: 1000 * 60 * 60 },
+//     });
+//     const data = await res.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 export default async function CompleteProfilePage() {
-  const teams = await fetchAllTeams();
-  return <CompleteProfilePageUI schoolOptions={teams} />;
+  // const teams = await fetchAllTeams();
+  return <CompleteProfilePageUI />;
 }
