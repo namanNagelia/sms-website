4d609b02f0edb5e0ab103065333bb8dfa6b409c6
namanNagelia <nagelia.naman@gmail.com>
2024-03-28 13:05:56 -0400

    Update route.ts

--- src/app/api/getAllTeams/route.ts
+++ src/app/api/getAllTeams/route.ts
@@ -6,7 +6,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 export async function GET(request: NextRequest) {
-  const school = await prisma.sms_organizations.findMany();
+  const school = await prisma.sms_org.findMany();
 
   return NextResponse.json({ school });
 }
